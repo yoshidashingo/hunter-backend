308c703d0ed5e87d4d697ca9ac3457da8b54ba0a
tsq <tangshiqiangcn@gmail.com>
2024-02-13 16:07:10 +0800

    add store query api

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import router from './router/index.js';
-
+import './mongodb.js';
 
 const app = express();
 
--- controller/storeController.js
+++ controller/storeController.js
@@ -1,3 +1,17 @@
+import Store from '../model/Store.js';
+
 export async function getStores(req, res) {
-    res.json({success: true});
+    const { page = 1, limit = 20 } = req.query;
+
+
+    const stores = await Store.find()
+        .limit(limit * 1)
+        .skip((page - 1) * limit)
+        .sort({ createdAt: -1 })
+    const count = await Store.countDocuments();
+
+    return res.json({
+        count,
+        stores
+    });
 }
