84dd65b92759fc0914f5b5347f55d105eaebf67f
tsq <tangshiqiangcn@gmail.com>
2024-02-15 09:15:04 +0800

    Update index.js

--- router/index.js
+++ router/index.js
@@ -1,8 +1,13 @@
 import apiRouter from './apiRouter.js';
 
 export default function router(app) {
+    app.get('/', (req, res) => {
+        res.json({success: true});
+    });
+
     app.get("/ping", (req, res) => {
         res.send("pong");
     });
+    
     app.use("/api", apiRouter);
 }
